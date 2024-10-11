70593fbf0de11b055745400d0aa8e6bfa284ad5c
Facundo <facurael@gmail.com>
2024-10-10 21:58:01 -0300

    feat: add CounterPage with increment and decrement buttons

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,32 @@
+'use client';
+import { useState } from "react";
+
+
+export const metadata = {
+ title: 'Counter Page',
+ description: 'Un simple contador',
+};
 
 export default function CounterPage() {
+
+  const [ count, setCount ] = useState(5);
+
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{ count }</span>   
+      <div className="flex">
+        <button
+          onClick={() => setCount( count + 1)} 
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+          +1
+        </button>
+        <button 
+          onClick={() => setCount( count - 1)} 
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+          -1
+        </button>    
+      </div>
     </div>
   );
 }
