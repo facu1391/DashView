8a90da85a5b06f1658ee7029dcbc43ecb0899fa1
Facundo <facurael@gmail.com>
2024-10-29 12:48:41 -0300

    feat: add initial counter state setup with initCounterState

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useAppDispatch, useAppSelector } from "@/store";
-import { addOne, substractOne } from "@/store/counter/counterSlice";
-import { useState } from "react";
+import { addOne, initCounterState, substractOne } from "@/store/counter/counterSlice";
+import { useEffect } from "react";
 
 interface Props {
     value?: number;
@@ -12,7 +12,10 @@ export const CartCounter = ({value = 0}: Props) => {
 
    const count = useAppSelector( state => state.counter.count );
    const dispatch = useAppDispatch();
-    //const [ count, setCount ] = useState(value);
+   
+    useEffect(() => {
+      dispatch( initCounterState(value) );
+    }, [dispatch, value])
 
   return (
     <>
