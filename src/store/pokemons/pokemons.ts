e838b11eda296d5f986ddbdd67f3252700362d20
Facundo <facurael@gmail.com>
2024-11-02 21:49:28 -0300

    feat: add toggleFavorites reducer and integrate favorite functionality in PokemonCard

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -1,5 +1,5 @@
 import { SimplePokemon } from '@/pokemons';
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface PokemonsState {
     [key: string]: SimplePokemon,
@@ -7,14 +7,26 @@ interface PokemonsState {
 
 const initialState: PokemonsState = {
     '1': { id: '1', name: 'bulbasaur'},
+    '3': { id: '3', name: 'ajkjakjsd'},
 }
 
 const pokemonsSlice = createSlice({
   name: 'pokemons',
   initialState,
-  reducers: {}
+  reducers: {
+    toggleFavorites( state, action: PayloadAction<SimplePokemon> ) {
+        const pokemon = action.payload;
+        const {id} = pokemon;
+        
+        if ( !!state[id] ) {
+            delete state[id];
+            return;
+        }
+        state[id] = pokemon;
+    }
+  }
 });
 
-export const {} = pokemonsSlice.actions;
+export const { toggleFavorites } = pokemonsSlice.actions;
 
 export default pokemonsSlice.reducer;
